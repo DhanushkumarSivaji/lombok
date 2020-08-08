32b081d9a2a0ba00f5fda0994898320de29059b0
DhanushkumarSivaji <dhanushkumar.s@yahoo.com>
2020-08-08 15:11:52 +0530

    Added swagger ui

--- src/main/java/com/dhanush/lombok/LombokApplication.java
+++ src/main/java/com/dhanush/lombok/LombokApplication.java
@@ -6,11 +6,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 //@ComponentScan("com.dhanush.lombok.*")
 @ComponentScan({"com.dhanush.lombok.controller","com.dhanush.lombok.service"})
 @EntityScan("com.dhanush.lombok.entity")
 @EnableJpaRepositories("com.dhanush.lombok.repository")
+@EnableSwagger2
 public class LombokApplication {
 
 	public static void main(String[] args) {
